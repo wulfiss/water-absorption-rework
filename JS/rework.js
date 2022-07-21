0931bd92e9bac44dee078f169805df63dc4c6308
wulfiss <96155980+wulfiss@users.noreply.github.com>
2022-07-21 09:01:37 -0300

    add some styling

--- JS/rework.js
+++ JS/rework.js
@@ -1,11 +1,11 @@
 let table = document.getElementById('tableResult')
 let tBody = document.getElementById('tbodyResult');
 
-for(let i = 0; i < 24; i++){
+for(let i = 0; i < 25; i++){
     let tr = tBody.insertRow();
-    for(let j = 0; j < 5; j++){
+    for(let j = 0; j < 6; j++){
         let td = tr.insertCell();
-        td.appendChild(document.createTextNode('----'));
+        td.appendChild(document.createTextNode('1234'));
     }
 }
 
