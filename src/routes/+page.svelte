217ba76c1bd353b4f19c76c586f58cf108466f19
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-24 17:18:16 -0300

    Clean code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,5 @@
 	import TableBody from '$lib/components/document/TableBody.svelte';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <Btn />
 <TableBody />
