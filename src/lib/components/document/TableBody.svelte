7863f7e1f2b9eb48c3ddc18a1b0d5790ce55d2d1
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-24 17:17:33 -0300

    Render table & some styling

--- src/lib/components/document/TableBody.svelte
+++ src/lib/components/document/TableBody.svelte
@@ -10,50 +10,57 @@
 </script>
 
 <div id="main">
-	<div>
-		Amount
-		{#each $sealsNumber as seal, i}
-			<div>{i + 1}</div>
-		{/each}
+	<div id="header">
+		<div class="cells">N° Pollo</div>
+		<div class="cells">Precinto</div>
+		<div class="cells">Peso Inicial</div>
+		<div class="cells">Peso Final</div>
+		<div class="cells">Diferencia</div>
+		<div class="cells">Hidratación</div>
 	</div>
+	{#each $sealsNumber as seal, i}
+		<div id="child" data-id={i}>
+			<div class="cells">{i + 1}</div>
+			<div class="cells">{seal}</div>
+			{#if !$initialWeights[i]}
+				<div class="cells">{'----'}</div>
+			{:else}
+				<div class="cells">{$initialWeights[i].toFixed(3)}</div>
+			{/if}
+			{#if !$finalWeights[i]}
+				<div class="cells">{'----'}</div>
+			{:else}
+				<div class="cells">{$finalWeights[i].toFixed(3)}</div>
+			{/if}
+			{#if !$differences[i]}
+				<div class="cells">{'----'}</div>
+			{:else}
+				<div class="cells">{$differences[i].toFixed(3)}</div>
+			{/if}
+			{#if !$percentages[i]}
+				<div class="cells">{'----'}</div>
+			{:else}
+				<div class="cells">{$percentages[i].toFixed(3)}</div>
+			{/if}
+		</div>
+	{/each}
 	<div>
-		seals
-		{#each $sealsNumber as seal}
-			<div>{seal}</div>
-		{/each}
+		{#if !$averageFinal}
+			<div>average: {'----'}</div>
+		{:else}
+			<div>average: {$averageFinal.toFixed(2)}</div>
+		{/if}
 	</div>
-	<div>
-		initial
-		{#each $initialWeights as initial}
-			<div>{initial.toFixed(3)}</div>
-		{/each}
-	</div>
-	<div>
-		final
-		{#each $finalWeights as final}
-			<div>{final.toFixed(3)}</div>
-		{/each}
-	</div>
-	<div>
-		difference
-		{#each $differences as difference}
-			<div>{difference.toFixed(3)}</div>
-		{/each}
-	</div>
-	<div>
-		percentages
-		{#each $percentages as percentage}
-			<div>{percentage.toFixed(2)}</div>
-		{/each}
-	</div>
-</div>
-<div>
-	average: {$averageFinal.toFixed(2)}
 </div>
 
 <style>
 	div#main {
+	}
+
+	div#child,
+	div#header {
 		display: grid;
-		grid-template-columns: repeat(7, 1fr);
+		grid-template-columns: repeat(6, 1fr);
+		justify-items: center;
 	}
 </style>
