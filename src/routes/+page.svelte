44cd5c57276472e65a7e070eb126c3b0823247f5
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-24 15:54:24 -0300

    Render table for test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script>
+	import Btn from '$lib/components/Testbtn/btn.svelte';
+	import TableBody from '$lib/components/document/TableBody.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Btn />
+<TableBody />
