b4b057fa4c42dff8aa090c0af4d4d1f74ed3e11f
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-24 19:32:08 -0300

    Add comments

--- src/lib/tools/swapElements.ts
+++ src/lib/tools/swapElements.ts
@@ -1,5 +1,7 @@
 import { random } from './random';
 
+/* sealLost(); generate an array that contain up to 4 number, between 0 and 19;
+this numbers are the "seals" that were lost during the test. */
 const sealLost = () => {
 	const x = random(3, 0);
 	const arr: number[] = [];
@@ -11,14 +13,16 @@ const sealLost = () => {
 	return arr;
 };
 
+/* swapContent takes the seals that were "lost" and its content with the 5 extras seals*/
+
 const swapContent = () => {
-	const EXTRA_SEAL_START = 20;
+	const EXTRA_SEAL_START = 20; //this number is where the extras seals start
 	const arr = sealLost();
 
 	for (let i = 0; i < arr.length; i += 1) {
 		const divOne = document.querySelector(`#child${i}`);
 		const divTwo = document.querySelector(`#child${i + EXTRA_SEAL_START}`);
-		const tmp = divOne.cloneNode(true);
+		const tmp = divOne.cloneNode(true); //make a copy of the node.
 
 		divOne.innerHTML = divTwo.innerHTML;
 		divTwo.innerHTML = tmp.innerHTML;
