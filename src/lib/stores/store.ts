8603396ee78cb1bfc58b52c8b4c3f5422312814f
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-24 03:00:35 -0300

    add store for average

--- src/lib/stores/store.ts
+++ src/lib/stores/store.ts
@@ -4,11 +4,11 @@ const Class = () => {
 	const temp: Writable<number[]> = writable([]);
 	return temp;
 };
-
+const averageFinal: Writable<number> = writable(0);
 const sealsNumber = Class();
 const percentages = Class();
 const initialWeights = Class();
 const finalWeights = Class();
 const differences = Class();
 
-export { sealsNumber, percentages, initialWeights, finalWeights, differences };
+export { sealsNumber, percentages, initialWeights, finalWeights, differences, averageFinal };
