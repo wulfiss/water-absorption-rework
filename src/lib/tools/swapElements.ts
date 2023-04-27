99ebcf48396c606d3fdf1894fb121eac3819eff3
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-27 14:18:38 -0300

    Fix Varible in loop, add ceil & types

--- src/lib/tools/swapElements.ts
+++ src/lib/tools/swapElements.ts
@@ -1,13 +1,14 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { random } from './random';
 
 /* sealLost(); generate an array that contain up to 4 number, between 0 and 19;
 this numbers are the "seals" that were lost during the test. */
 const sealLost = () => {
-	const x = random(3, 0);
+	const x = Math.ceil(random(3, 0));
 	const arr: number[] = [];
 
 	for (let i = 0; i <= x; i += 1) {
-		arr.push(random(19, 0));
+		arr.push(Math.ceil(random(19, 0)));
 	}
 
 	return arr;
@@ -20,9 +21,10 @@ const swapContent = () => {
 	const arr = sealLost();
 
 	for (let i = 0; i < arr.length; i += 1) {
-		const divOne = document.querySelector(`#child${i}`);
-		const divTwo = document.querySelector(`#child${i + EXTRA_SEAL_START}`);
-		const tmp = divOne.cloneNode(true); //make a copy of the node.
+		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+		const divOne = document.querySelector<HTMLDivElement>(`#child${arr[i]}`)!;
+		const divTwo = document.querySelector<HTMLDivElement>(`#child${i + EXTRA_SEAL_START}`)!;
+		const tmp = divOne.cloneNode(true) as HTMLDivElement; //make a copy of the node.
 
 		divOne.innerHTML = divTwo.innerHTML;
 		divTwo.innerHTML = tmp.innerHTML;
