e59649b5faf0547d95b077dd68ff7126cc4f166a
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-27 18:36:37 -0300

    Add date and time

---
+++ src/lib/components/document/dateTime.svelte
@@ -0,0 +1,18 @@
+<div id="main">
+	<div id="date">Fecha: 27-04-20023</div>
+	<div id="time">
+		<div>Hora de inicio: 04:20 HS</div>
+		<div>Hora de finalización: 05:08 HS</div>
+	</div>
+</div>
+
+<style lang="scss">
+	div#main {
+		display: grid;
+		height: 80px;
+		grid-template-columns: repeat(2, 1fr);
+		align-content: center;
+		font-weight: bold;
+		font-size: 13px;
+	}
+</style>
