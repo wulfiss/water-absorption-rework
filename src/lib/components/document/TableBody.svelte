ec5f427ff969ed65dad27b122d3256e750192ee6
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-27 20:11:55 -0300

    some small changes

--- src/lib/components/document/TableBody.svelte
+++ src/lib/components/document/TableBody.svelte
@@ -11,12 +11,12 @@
 
 <div id="main">
 	<div id="header">
-		<div class="cells">N° Pollo</div>
-		<div class="cells">Precinto</div>
-		<div class="cells">Peso Inicial</div>
-		<div class="cells">Peso Final</div>
-		<div class="cells">Diferencia</div>
-		<div class="cells">Hidratación</div>
+		<div class="cells">N° de Pollos</div>
+		<div class="cells">N° de Precinto</div>
+		<div class="cells">Peso Inicial(kg)</div>
+		<div class="cells">Peso Final(kg)</div>
+		<div class="cells">Diferencia(kg)</div>
+		<div class="cells">% Hidratación</div>
 	</div>
 	<div class="tableBody">
 		<div class="sealsGroup">
@@ -50,7 +50,7 @@
 					{#if !$percentages[i]}
 						<div class="cells">----</div>
 					{:else}
-						<div class="cells">{$percentages[i].toFixed(3)}</div>
+						<div class="cells">{$percentages[i].toFixed(2)}</div>
 					{/if}
 				</div>
 			{/each}
@@ -59,6 +59,12 @@
 </div>
 
 <style lang="scss">
+	%cells {
+		border: 1px solid black;
+		width: 100%;
+		height: 100%;
+		text-align: center;
+	}
 	div.tableBody {
 		display: grid;
 		grid-template-columns: repeat(6, 1fr);
@@ -69,6 +75,27 @@
 		grid-column-start: 1;
 		grid-column-end: 2;
 		justify-items: center;
+		border: 1px solid black;
+		width: 100%;
+		margin-left: -1px;
+		margin-top: -1px;
+		padding: 0;
+
+		.seals {
+			height: 19.11px;
+			@extend %cells;
+			border-left: 0;
+			border-top: 0;
+			border-right: 0;
+		}
+		:last-child {
+			margin-bottom: 0px;
+			border-bottom: 0;
+		}
+
+		:first-child {
+			border-top: 0;
+		}
 	}
 
 	div.childGroup {
@@ -81,11 +108,30 @@
 		display: grid;
 		grid-template-columns: repeat(6, 1fr);
 		justify-items: center;
+		border-top: 1px solid black;
+		font-weight: bold;
+
+		:first-child {
+			border-bottom: 0;
+			border-left: 1px solid black;
+			height: 20.89px;
+		}
 	}
 
 	div.child {
 		display: grid;
 		grid-template-columns: repeat(5, 1fr);
 		justify-items: center;
+	}
+
+	div#main {
+		width: 648px;
+		font-size: 15px;
+	}
+
+	div.cells {
+		@extend %cells;
+		border-top: 0;
+		border-left: 0;
 	}
 </style>
