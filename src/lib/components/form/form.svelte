56378de5655343dbc6be41d64114e0aabaaf3b2e
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-29 16:30:49 -0300

    Add material styling

--- src/lib/components/form/form.svelte
+++ src/lib/components/form/form.svelte
@@ -1,25 +1,27 @@
 <script>
 	import Btn from '../Testbtn/btn.svelte';
 	import Button, { Label } from '@smui/button';
-
-	let clicked = 0;
-
+	import HelperText from '@smui/textfield/helper-text';
 	import Textfield from '@smui/textfield';
 	import Icon from '@smui/textfield/icon';
-	import HelperText from '@smui/textfield/helper-text';
-	import Paper, { Title, Content } from '@smui/paper';
+	import Paper, { Content } from '@smui/paper';
 	import Radio from '@smui/radio';
 	import FormField from '@smui/form-field';
 	import Checkbox from '@smui/checkbox';
+	import TopAppBar, { Row, Section, Title } from '@smui/top-app-bar';
 
 	let checked = false;
 	let checked2 = false;
+	let clicked = 0;
+
+	let date = '';
+	let time = '';
+	let average = 0;
+	let customPercent = 0;
 
 	let value = '';
 	let valueA = '';
 	let valueB = '';
-	let valueC = '';
-	let valueD = '';
 	let options = [
 		{
 			name: 'SENASA (8%)',
@@ -38,56 +40,89 @@
 </script>
 
 <div id="formDiv">
-	<Paper color="primary" variant="outlined">
-		<Title>Generador de Registro de Hidratación</Title>
+	<Paper color="primary" variant="outlined" class="paper">
+		<div class="top-app-bar-container" style="text-align:center">
+			<TopAppBar variant="static">
+				<Row>
+					<Section>
+						<Title Style="margin-left: 23px">Generador de Registro de Hidratación</Title>
+					</Section>
+				</Row>
+			</TopAppBar>
+		</div>
+		<br />
 		<Content>
-			<div>
-				<div>
-					<Textfield bind:value={valueB} label="Fecha" type="date" style="width: 121px;">
-						<Icon class="material-icons" slot="leadingIcon">event</Icon>
-					</Textfield>
-				</div>
-				<div>
-					<Textfield bind:value={valueB} label="Hora de inicio" type="time" style="width: 121px;">
-						<Icon class="material-icons" slot="leadingIcon">alarm_on</Icon>
-					</Textfield>
-				</div>
-				<div class="columns margins">
+			<div id="content">
+				<div id="first">
 					<div>
-						<Textfield
-							bind:value={valueA}
-							label="Peso promedio de pollos"
-							suffix="㎏"
-							input$pattern="\d+"
-							type="number"
-						/>
+						<Textfield bind:value={date} label="Fecha" type="date" style="width: 131px;">
+							<Icon class="material-icons" slot="leadingIcon">event</Icon>
+						</Textfield>
+					</div>
+					<div>
+						<Textfield bind:value={time} label="Hora de inicio" type="time" style="width: 131px;">
+							<Icon class="material-icons" slot="leadingIcon">alarm_on</Icon>
+						</Textfield>
+					</div>
+					<div class="columns margins">
+						<div>
+							<Textfield
+								class="average"
+								bind:value={average}
+								label="Peso promedio"
+								suffix="㎏"
+								input$pattern="\d+"
+								type="number"
+								style="width: 131px;"
+							>
+								<HelperText slot="helper" style="margin-left: 150px;">ej. 2.50</HelperText>
+							</Textfield>
+						</div>
 					</div>
 				</div>
 				<div class="radio-demo">
 					<p>Limite de Hidratación(%)</p>
-					<FormField>
-						<Radio bind:group={selected} value={options[0].name} disabled={options[0].disabled} />
-						<span>
+					<FormField class="radio">
+						<Radio
+							class="radioChild"
+							bind:group={selected}
+							value={options[0].name}
+							disabled={options[0].disabled}
+						/>
+						<span class="radioSpan">
 							{options[0].name}
 						</span>
-						<Radio bind:group={selected} value={options[1].name} disabled={options[1].disabled} />
-						<span>
+						<Radio
+							class="radioChild"
+							bind:group={selected}
+							value={options[1].name}
+							disabled={options[1].disabled}
+						/>
+						<span class="radioSpan">
 							{options[1].name}{options[1].disabled ? ' (disabled)' : ''}
 						</span>
-						<Radio bind:group={selected} value={options[2].name} disabled={options[2].disabled} />
-						<span>
-							{options[2].name}{options[2].disabled ? ' (disabled)' : ''}
+						<Radio
+							class="radioChild"
+							bind:group={selected}
+							value={options[2].name}
+							disabled={options[2].disabled}
+						/>
+						<span class="spanLast">
 							<Textfield
-								bind:value={valueB}
-								label="%"
+								bind:value={customPercent}
+								label="Otro"
+								label$style="margin-top: 5px; font-size: 14px"
+								suffix="%"
+								input$pattern="\d+"
 								type="number"
-								disabled
-								style="width: 121px;"
-							/>
+								style="width: 70px;"
+							>
+								<HelperText slot="helper">ej. 4.3</HelperText>
+							</Textfield>
 						</span>
 					</FormField>
 				</div>
-				<div style="margin-top: 1em;">
+				<div id="seals">
 					<FormField align="end">
 						<Checkbox bind:checked={checked2} />
 						<span slot="label">Agregar precintos perdidos</span>
@@ -95,18 +130,19 @@
 				</div>
 				<div class="margins">
 					<Textfield
-						style="width: 100%;"
-						helperLine$style="width: 100%;"
 						textarea
 						bind:value
 						label="Observaciones"
+						input$rows={4}
+						input$cols={50}
+						input$resizable={false}
 					/>
 				</div>
 				<Button on:click={() => clicked++} variant="raised">
 					<Label>Raised</Label>
 				</Button>
-			</div></Content
-		>
+			</div>
+		</Content>
 	</Paper>
 </div>
 
@@ -119,24 +155,33 @@
 		justify-content: center;
 		align-items: center;
 		text-align: center;
-	}
-	form {
-		border: 1px solid black;
-		border-radius: 14px;
-		width: 50dvw;
-		height: 80dvh;
-	}
-	ul,
-	li {
-		text-decoration: none;
-		list-style: none;
+		background-color: #ff8a65;
 	}
 
-	.textField {
-		width: 121px;
+	div#content {
+		display: grid;
+		row-gap: 10px;
+	}
+
+	* :global(.paper) {
+		background-color: white;
+		height: 90vh;
+		height: 90dvh;
 	}
 
 	.radio-demo > :global(*) {
 		margin: 0 0.2em;
+	}
+
+	* :global(.radio) {
+		height: 10dvh;
+	}
+
+	* :global(.radioChild) {
+		margin-top: -10px;
+	}
+
+	.radioSpan {
+		margin-top: -10px;
 	}
 </style>
