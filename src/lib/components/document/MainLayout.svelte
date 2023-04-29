eabf38c3ea981cc43cdfa61434873b6f209546de
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-29 17:24:16 -0300

    change some visuals

--- src/lib/components/document/MainLayout.svelte
+++ src/lib/components/document/MainLayout.svelte
@@ -5,29 +5,63 @@
 	import Average from './components/average.svelte';
 	import TextBox from './components/textBox.svelte';
 	import Signatures from './components/signatures.svelte';
+	import Button, { Label } from '@smui/button';
 </script>
 
-<div id="document">
-	<Header />
-	<DateTime />
-	<TableBody />
-	<Average />
-	<TextBox />
-	<Signatures />
+<div id="pageComplete">
+	<div id="btn">
+		<Button variant="raised">
+			<Label>Imprimir</Label>
+		</Button>
+	</div>
+	<div id="outer">
+		<div id="document">
+			<Header />
+			<DateTime />
+			<TableBody />
+			<Average />
+			<TextBox />
+			<Signatures />
+		</div>
+	</div>
 </div>
 
 <style lang="scss">
 	* {
 		box-sizing: content-box; /* 1 */
-		height: 0; /* 1 */
 		overflow: visible; /* 2 */
+	}
+
+	div#btn {
+		display: block;
+		position: sticky;
+		right: 1050px;
+		top: 300px;
+	}
+
+	div#pageComplete {
+		width: 100vw;
+		width: 100dvw;
+		height: 1400px;
+		background-color: #ff8a65;
+		display: grid;
+		justify-items: center;
+		align-content: center;
 	}
 
 	#document {
 		font-family: Calibri, 'Gill Sans', 'Gill Sans MT', 'Trebuchet MS';
 		width: 210mm;
 		height: 297mm;
-		margin: 45px 35px;
+	}
+
+	#outer {
+		background-color: white;
+		height: 1240px;
+		width: 880px;
+		display: grid;
+		justify-content: center;
+		align-content: center;
 	}
 
 	@media print {
