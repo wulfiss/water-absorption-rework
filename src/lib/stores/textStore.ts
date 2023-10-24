582a916dd73626eafcd38f924ad46bee0796f55e
Julian <96155980+wulfiss@users.noreply.github.com>
2023-10-24 20:29:36 -0300

    Clean a little

--- src/lib/stores/textStore.ts
+++ src/lib/stores/textStore.ts
@@ -1,17 +1,37 @@
 import { type Readable, readable } from 'svelte/store';
 
 export const textStore = readable({
-	enercop: {
-		brandTitle: 'POLLOS ENERCOP',
+	0: {
+		brandTitle: '',
 		brandSubTitle: 'Planta Procesadora de Aves',
-		logoBrand: '/src/lib/images/logoenercop.jpg',
 		officialName: 'ESTABLECIMIENTO OFICIAL N° 1612',
 		companyName: 'Unión Agricola de Avellaneda',
-		companyLogo: '/src/lib/images/logouaa.jpg',
 		documentTitle: 'Determinación de Absorción de Agua',
 		documentCode: 'RH-HIDRA-006',
 		documentValidity: '01/10/21',
 		documentRevision: '04',
+		documentPages: '1 de 1'
+	},
+	1: {
+		brandTitle: '',
+		brandSubTitle: 'Planta Procesadora de Aves',
+		officialName: 'ESTABLECIMIENTO OFICIAL N° 1612',
+		companyName: 'Unión Agricola de Avellaneda',
+		documentTitle: 'Determinación de Absorción de Agua',
+		documentCode: 'RH-HIDRA-006',
+		documentValidity: '01/10/21',
+		documentRevision: '04',
+		documentPages: '1 de 1'
+	},
+	2: {
+		brandTitle: '',
+		brandSubTitle: 'Planta Procesadora de Aves',
+		officialName: 'ESTABLECIMIENTO OFICIAL N° 1612',
+		companyName: 'Unión Agricola de Avellaneda',
+		documentTitle: 'Determinación de Absorción de Solución de Aditivado',
+		documentCode: 'RH-XXX-XXX',
+		documentValidity: 'XX/XX/XX',
+		documentRevision: '00',
 		documentPages: '1 de 1'
 	}
 });
