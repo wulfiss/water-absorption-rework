44a7cdbefbb8783e60b6249730642c3825b7e412
wulfiss <96155980+wulfiss@users.noreply.github.com>
2023-10-25 10:47:07 -0300

    Reduce names

--- src/lib/stores/variableStore.ts
+++ src/lib/stores/variableStore.ts
@@ -1,19 +1,19 @@
 import { type Readable, readable } from 'svelte/store';
 
-export const variableStore = readable({
+export const variables = readable({
 	0: {
 		probes: 25,
-		userTotalProbes: 20,
+		userProbes: 20,
 		percentage: 4.5
 	},
 	1: {
 		probes: 25,
-		userTotalProbes: 20,
+		userProbes: 20,
 		percentage: 8
 	},
 	2: {
 		probes: 10,
-		userTotalProbes: 10,
+		userProbes: 10,
 		percentage: 10
 	}
 });
