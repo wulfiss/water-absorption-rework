703d55f9c267c7a7ca7b2cd5c6f156ef45bcfe0d
wulfiss <96155980+wulfiss@users.noreply.github.com>
2023-12-06 08:36:16 -0300

    add variables for time

--- src/lib/stores/variableStore.ts
+++ src/lib/stores/variableStore.ts
@@ -4,16 +4,22 @@ export const variables = readable({
 	0: {
 		maxProbes: 25,
 		probes: 20,
-		percentage: 4.5
+		percentage: 4.5,
+		timeMax: 50,
+		timeMin: 45
 	},
 	1: {
 		maxProbes: 25,
 		probes: 20,
-		percentage: 8
+		percentage: 8,
+		timeMax: 50,
+		timeMin: 45
 	},
 	2: {
 		maxProbes: 10,
 		probes: 10,
-		percentage: 10
+		percentage: 10,
+		timeMax: 15,
+		timeMin: 9
 	}
 });
